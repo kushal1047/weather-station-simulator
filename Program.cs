cba0cf99871c1fe1f1b7342dd0296d12f422b7f8
Kushal Niraula <kushal463334@gmail.com>
2024-08-19 12:32:00 +1000

    Initialize required data for the program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace WeatherStationSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of days you want to simulate.");
+            int days = int.Parse(Console.ReadLine());
+            int[] temperatures = new int[days];
+            string[] conditions = ["Sunny", "Cloudy", "Rainy", "Snowy"];
+            string[] weatherConditions = new string[days];
+            Random random = new Random();
+            for (int i = 0; i < days; i++)
+            {
+                temperatures[i] = random.Next(-10, 40);
+                weatherConditions[i] = conditions[random.Next(conditions.Length)];
+            }
+        }
+    }
+}
